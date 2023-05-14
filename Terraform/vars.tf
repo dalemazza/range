e0b294ef4bdf0662be14a6a13decb39aa8d66a2d
dalemazza <66513243+dalemazza@users.noreply.github.com>
2023-05-14 10:29:34 +0100

    Made the vars.tf file
    Added all the vars needed to create the servers and workstations

---
+++ Terraform/vars.tf
@@ -0,0 +1,15 @@
+variable "proxmox_host" {
+    default = "proxmox.local"
+}
+variable "win10_template" {
+    default = "win10_template"
+}
+variable "win_server_template" {
+    default = "server_16_template"
+}
+variable "wkst_ram" {
+    default = "4096"
+}
+variable "server_ram" {
+    default = "8192"
+}
