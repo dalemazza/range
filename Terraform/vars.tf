2181fe6b920e87dfd093a31838620e53e38d6434
dalemazza <66513243+dalemazza@users.noreply.github.com>
2023-05-14 16:26:48 +0100

    Update vars.tf
    Added descriptions to all the vars.

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,4 +1,4 @@
-variable "proxmox_host" {
+variable "pm_host" {
     default = "proxmox.local"
 }
 variable "win10_template" {
@@ -8,8 +8,21 @@ variable "win_server_template" {
     default = "server_16_template"
 }
 variable "wkst_ram" {
+    description = "Enter the amount if ram you would like on the workstations"
     default = "4096"
 }
 variable "server_ram" {
+    description = "Enter the amount of ram on the servers you would like"
     default = "8192"
+}
+variable "win_server_amount" {
+    description = "Enter the amount of windows servers you want creating"
+    default = "1"
+}
+variable "win_workstation_amount" {
+
+    description = "Enter the amount of windows workstations you want creating"
+
+    default = "1"
+
 }
