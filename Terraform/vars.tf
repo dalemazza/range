704fc4d861c63dd3199bfa8a8a91c847963393ea
dalemazza <dalemarriott@live.co.uk>
2023-05-14 20:30:39 +0100

    sorting out tf

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,5 +1,5 @@
 variable "pm_host" {
-    default = "proxmox.local"
+    default = "192.168"
 }
 variable "win10_template" {
     default = "win10_template"
@@ -20,9 +20,28 @@ variable "win_server_amount" {
     default = "1"
 }
 variable "win_workstation_amount" {
-
     description = "Enter the amount of windows workstations you want creating"
-
     default = "1"
-
+}
+variable "server_ips" {
+  description = "List of ip addresses for master nodes"
+  type        = list(string)
+  default     = [
+    "192.168.1.128",
+  ]
+}
+variable "workstations_ips" {
+  description = "List of ip addresses for worker nodes"
+  type        = list(string)
+  default     = [  
+    "192.168.1.130",
+  ]
+}
+variable "networkrange" {
+  description = "Enter as 8,16,22,24,etc. hint: 10.0.0.0/8"
+  default = 192.168.1.0/24
+}
+variable "gateway" {
+  description = "Enter your network gateway."
+  default = "192.168.1.1"
 }
